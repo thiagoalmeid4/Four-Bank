b1b3b31fb9784fdad4da7c5e287da0449dd462fd
Thiago Almeida <thiago_dev@companye.academy>
2023-12-29 15:41:03 -0400

    update function

--- functions/obter_saldo_conta.sql
+++ functions/obter_saldo_conta.sql
@@ -3,21 +3,22 @@
 -- DROP FUNCTION IF EXISTS public.obter_saldo_conta(bigint);
 
 CREATE OR REPLACE FUNCTION public.obter_saldo_conta(
-	p_id_usuario bigint)
-    RETURNS TABLE(saldo_conta numeric, nr_conta character varying, nr_agencia character varying) 
-    LANGUAGE 'plpgsql'
-    COST 100
-    VOLATILE PARALLEL UNSAFE
-    ROWS 1000
+    p_id_usuario bigint)
+RETURNS TABLE(saldo_conta numeric, nr_conta character varying, nr_agencia character varying, nome_cliente character varying) 
+LANGUAGE 'plpgsql'
+COST 100
+VOLATILE PARALLEL UNSAFE
+ROWS 1000
 
 AS $BODY$
 DECLARE
     v_saldo_conta numeric(38,2);
 BEGIN
     -- Obter saldo da conta
-    SELECT c.vl_saldo_conta, c.nr_conta, c.nr_agencia
-    INTO v_saldo_conta, nr_conta, nr_agencia
+    SELECT c.vl_saldo_conta, c.nr_conta, c.nr_agencia, cl.nm_cliente
+    INTO v_saldo_conta, nr_conta, nr_agencia, nome_cliente
     FROM tb_conta c
+    JOIN tb_cliente cl ON c.fk_nr_id_cliente = cl.nr_id_cliente
     WHERE c.fk_nr_id_cliente = p_id_usuario;
 
     -- Verificar transações do tipo 1 dentro de um minuto
@@ -37,10 +38,11 @@ BEGIN
             AND t.dt_transacao::timestamp >= (CURRENT_TIMESTAMP - interval '1 minute')::timestamp;
     END IF;
 
-    RETURN QUERY SELECT v_saldo_conta, nr_conta, nr_agencia;
+    RETURN QUERY SELECT v_saldo_conta, nr_conta, nr_agencia, nome_cliente;
 
 END;
 $BODY$;
+
 
 ALTER FUNCTION public.obter_saldo_conta(bigint)
     OWNER TO postgres;
